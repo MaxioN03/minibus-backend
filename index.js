df6fadcbb1a4199ba290bd74b70e30dc06ca24a3
MaxioN03 <maxion03@gmail.com>
2021-06-14 11:01:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 const routes = require('./src/routes');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
